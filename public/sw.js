ecc3ed4b0553cd9443150fe3fc0c2d70dbd52cd7
zphyrj <46908127+zphyrj@users.noreply.github.com>
2022-01-05 02:22:35 -0800

    switched to relative paths

--- public/sw.js
+++ public/sw.js
@@ -1,20 +1,65 @@
-self.addEventListener("install", function (e) {
-  e.waitUntil(
-    caches.open("airhorner").then(function (cache) {
+CACHE_NAME = 'pwa-starter-template';
+CACHE_VERSION = 'v20';
+
+self.addEventListener('install', function(event) {
+  self.skipWaiting();
+  event.waitUntil( async () => {
+    if (self.registration.navigationPreload) {
+      // Enable navigation preloads!
+      await self.registration.navigationPreload.enable();
+    }
+    await caches.open(CACHE_NAME).then(function(cache) {
       return cache.addAll([
-        "/",
-        "/index.html"
+        '/mvhs-timer/',
+        '/mvhs-timer/index.html',
       ]);
     })
-  );
+  });
 });
 
-self.addEventListener("fetch", function (event) {
-  console.log(event.request.url);
+self.addEventListener('activate', () => {
+  caches.keys().then(function(keyList) {
+    console.log(keyList);
+    keyList.forEach(key=> caches.delete(key));
+  });
+});
 
-  event.respondWith(
-    caches.match(event.request).then(function (response) {
-      return response || fetch(event.request);
-    })
-  );
+
+self.addEventListener('fetch', function(event) {
+
+  async function getCachedResponse(url) {
+    const response = await caches.match(url);
+    if (response) {
+      return response;
+    }
+    return new Response('Service worker fetch error', {
+      status: 500,
+      statusText: 'Service worker fetch error'
+    });
+  }
+
+  function getFromServer(url) {
+    const response = fetch(url)
+      .then((response) => {
+        if (!response || response.status !== 200 || response.type !== 'basic') {
+          console.log('loading from cache due to bad response from server ' + event.request.url);
+          return caches.match(url);
+        }
+        return response;
+      })
+    return response;
+  }
+
+  event.respondWith((async () => {
+    const url = event.request.url;
+    const cachedResponse = await getCachedResponse(url);
+    if (cachedResponse && cachedResponse.status === 200) {
+      return cachedResponse;
+    }
+    const serverResponse = await getFromServer(url);
+    const cache = await caches.open(CACHE_NAME);
+    const clonedResponse = serverResponse.clone();
+    cache.put(url, clonedResponse);
+    return serverResponse;
+  })());
 });
